5214132fc0bedd35ed7d5b481b3ad7b5ad877cf1
mrcaption49 <pranavsb49@gmail.com>
2025-10-02 23:50:44 +0530

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -178,6 +178,9 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR /'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'   # or os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
